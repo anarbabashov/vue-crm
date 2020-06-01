57685ebd3a931ba3e7fb58a355221de4aa51f3b3
Anar Babashov <anar@anattadesign.com>
2020-06-01 15:05:10 -0700

    Created a message plugin.

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
+import messagePlugin from '@/utils/message.plugin'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize'
 
-Vue.use(Vuelidate);
-
 Vue.config.productionTip = false
 
+Vue.use(messagePlugin);
+Vue.use(Vuelidate);
 Vue.filter('date', dateFilter);
 
 new Vue({
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -58,6 +58,7 @@
 
 <script>
 import {email, required, minLength} from 'vuelidate/lib/validators';
+import messages from '@/utils/messages';
 export default {
   namer: 'Login',
   data: () => ({
@@ -68,16 +69,21 @@ export default {
     email: {email, required},
     password: {required, minLength: minLength(6)}
   },
+  mounted() {
+    if (messages[this.$route.query.message]) {
+      this.$message(messages[this.$route.query.message])
+    }    
+  },
   methods: {
     submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch()
         return
       }
-      const formData = {
-        email: this.email,
-        password: this.password
-      }
+      // const formData = {
+      //   email: this.email,
+      //   password: this.password
+      // }
       console.log(formData);
       this.$router.push('/')
     }
