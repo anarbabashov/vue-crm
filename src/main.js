7edcb5d05a7331fc73fb7fc6b5a2cdcd678afea5
Anar Babashov <anar@anattadesign.com>
2020-06-01 17:25:42 -0700

    Adding Firebase. Login form authorization.

--- src/main.js
+++ src/main.js
@@ -8,14 +8,34 @@ import messagePlugin from '@/utils/message.plugin'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false
 
 Vue.use(messagePlugin);
 Vue.use(Vuelidate);
 Vue.filter('date', dateFilter);
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: "AIzaSyAlQ4ncBfRf0IyQkfNHHMmGFvggGoVX5XI",
+  authDomain: "vue-crm-335a9.firebaseapp.com",
+  databaseURL: "https://vue-crm-335a9.firebaseio.com",
+  projectId: "vue-crm-335a9",
+  storageBucket: "vue-crm-335a9.appspot.com",
+  messagingSenderId: "1072033480245",
+  appId: "1:1072033480245:web:67f620a22f037c807bfb7c"
+})
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -75,17 +75,19 @@ export default {
     }    
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch()
         return
       }
-      // const formData = {
-      //   email: this.email,
-      //   password: this.password
-      // }
-      console.log(formData);
-      this.$router.push('/')
+      const formData = {
+        email: this.email,
+        password: this.password
+      }
+      try {
+        await this.$store.dispatch('login', formData)
+        this.$router.push('/') 
+      } catch (e) {}
     }
   }
 }
