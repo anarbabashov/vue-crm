79db70d77b30a1459921b4d14e8f68ec8015d1d0
Anar Babashov <anar@anattadesign.com>
2020-06-01 18:35:19 -0700

    Sign up through Firebase.

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -91,7 +91,7 @@ export default {
     }
   },
   methods: {
-    submitHaldler() {
+    async submitHaldler() {
       if (this.$v.$invalid) {
         this.$v.$touch()
         return
@@ -101,8 +101,11 @@ export default {
         password: this.password,
         name: this.name
       }
-      console.log(formData)
-      this.$router.push('/')     
+
+      try {
+        await this.$store.dispatch('register', formData)
+        this.$router.push('/')
+      } catch (e) {}  
     }
   }
 }
